55a6bf79c54b2e47331ad2c14af0cd416724e67b
Haru_Tran <haru.tran.0824@student.uu.se>
2024-02-08 18:02:14 +0100

    Listgroups
    Co-authored-by: Emil  <EmDe99@users.noreply.github.com>

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+//https://babeljs.io/
+
+// PascalCasing
+function Message() {
+  const name = "s";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello world</h1>;
+}
+
+export default Message;
